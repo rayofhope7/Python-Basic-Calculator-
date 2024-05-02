f215cd93edb57b599d2819c1ded808b8c8a7f90d
Anuj Singh Mauryavanshi <96892558+rayofhope7@users.noreply.github.com>
2024-05-02 14:17:57 +0530

    Add files via upload

---
+++ Python Basic Calculator.py
@@ -0,0 +1,7 @@
+#Basic Python Calculator
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("The sum of a+b:", a + b)
+print("The subtraction of a-b:", a - b)
+print("The subtraction of a-b:", a * b)
+print("The subtraction of a-b:", a / b)
